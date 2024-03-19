e1cb2e8700c3896ea999c552c18aaac4a761eb75
samwondim <samwondim@gmail.com>
2024-03-19 09:55:58 +0300

    add common user logic for authentication for users.

--- pharma_care/main/admin.py
+++ pharma_care/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from main.models import User, Pharmacist, PharmacyTechnician, InventoryManager
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+
+admin.site.register(Pharmacist)
+admin.site.register(PharmacyTechnician)
+admin.site.register(InventoryManager)
