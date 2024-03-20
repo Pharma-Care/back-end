045e619b42b45ba9b016b937fd31e77b8340b7e1
samwondim <samwondim@gmail.com>
2024-03-20 15:19:41 +0300

    added additional apps

--- pharma_care/app/settings.py
+++ pharma_care/app/settings.py
@@ -33,6 +33,11 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "main",
+    "chat",
+    "prescription",
+    "orders",
+    "notification",
+    "inventory",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
