71e70a0b27e3d706465027f282aee85120b3b50b
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2024-03-28 17:08:01 +0300

    authentication for inventory access added

--- pharma_care/inventory/views.py
+++ pharma_care/inventory/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets, filters
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from main.serializers import InventorySerializer 
 from main.models import InventoryItem  
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
     queryset = InventoryItem.objects.all()
     serializer_class = InventorySerializer
-    
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
